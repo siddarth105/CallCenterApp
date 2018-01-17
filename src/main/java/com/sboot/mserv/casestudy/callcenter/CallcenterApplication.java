6b419a941dc6b4326ffc70cc469f0f6c7398e616
siddarth105 <siddarth.a.t105@gmail.com>
2018-01-17 22:12:52 +0530

    for external tomcat

--- src/main/java/com/sboot/mserv/casestudy/callcenter/CallcenterApplication.java
+++ src/main/java/com/sboot/mserv/casestudy/callcenter/CallcenterApplication.java
@@ -2,15 +2,23 @@ package com.sboot.mserv.casestudy.callcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 /*@Configuration
 @EnableAutoConfiguration
 @ComponentScan({"com.sboot.mserv.casestudy.callcenter.resource"})*/
-@SpringBootApplication
-public class CallcenterApplication {
+@SpringBootApplication(exclude = {EmbeddedServletContainerAutoConfiguration.class})
+public class CallcenterApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CallcenterApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+		return app.sources(CallcenterApplication.class);
 	}
 }
